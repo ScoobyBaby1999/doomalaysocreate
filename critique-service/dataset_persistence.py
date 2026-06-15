5987cb4e7f1071459ffae4ba8d52bf0c7b863917
ScoobyBaby1999 <firstdoobievault@gmail.com>
2026-06-15 06:54:50 +0300

    fix: HF OAuth scope should be write-repos not write

--- critique-service/dataset_persistence.py
+++ critique-service/dataset_persistence.py
@@ -33,7 +33,7 @@ import db
 HF_AUTHORIZE_URL = "https://huggingface.co/oauth/authorize"
 HF_TOKEN_URL = "https://huggingface.co/oauth/token"
 HF_API_BASE = "https://huggingface.co"
-HF_SCOPES = "openid profile write"
+HF_SCOPES = "openid profile write-repos"
 
 # ---------------------------------------------------------------------------
 # HF API helpers
